a7b0895505c00bf1c99d0160cb5bf2779596655a
Aryan Malik <aryanhunny98@gmail.com>
2024-02-11 21:30:42 +0530

    Got Navbar created

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,45 @@
+import Image from "next/image";
+
 export const Navbar = () => {
     return (
         <nav className="flex justify-between items-center p-5 bg-purple-800">
-        <div className="flex-row text-white">
-            <h1 className="text-4xl font-bold">Jane Doe</h1>
-            <div className="text-sm">
+
+            {/* Name Block */}
+            <div className="flex-row text-white">
                 
-                <p>jdoe931@gmail.com</p>
+                {/* Name */}
+                <h1 className="text-4xl font-bold">Jane Doe</h1>
+
+                {/* Email */}
+                <div className="flex text-sm gap-3">
+                    <Image 
+                        src="/email-icon.png" 
+                        alt="Email_Icon"
+                        width={24}
+                        height={24}
+                    />
+                    <p>jdoe931@gmail.com</p>
+                </div>
+
+                {/* Phone Number */}
+                <div className="flex text-sm pl-1 gap-3">
+                    <Image 
+                        src="/phone-icon.png" 
+                        alt="Phone_Icon"
+                        width={15}
+                        height={15}
+                    />
+                    <p>+123-456-7890</p>
+                </div>
             </div>
-        </div>
-        <div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Login</button>
-        </div>
+
+            {/* Links */}
+            <div className="flex gap-14 text-gray-300">
+                <a href="/" className="hover:underline hover:font-bold">About Me</a>
+                <a href="/" className="hover:underline hover:font-bold">Skills</a>
+                <a href="/" className="hover:underline hover:font-bold">Projects</a>
+                <a href="/" className="hover:underline hover:font-bold">Recommendations</a>
+            </div>
         </nav>
     );
 }
